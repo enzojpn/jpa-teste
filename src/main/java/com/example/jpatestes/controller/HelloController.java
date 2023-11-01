6b5d0ac484f5a410005814ab1937b72198f21134
enzojpn <enzo_jpn@hotmail.com>
2023-10-31 21:39:18 -0300

    criando hello controller

---
+++ src/main/java/com/example/jpatestes/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.jpatestes.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping
+    public String hello() {
+        logger.info("tteste");
+        return "hello";
+    }
+
+}
---
+++ src/test/java/com/example/jpatestes/JpaTestesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpatestes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
